849beeebfd1b651571826b806ed7e49121618272
dehbi16 <dehbi16@gmail.com>
2020-04-29 19:22:35 +0200

    modifier game

--- Banana/src/Game.java
+++ Banana/src/Game.java
@@ -1,4 +1,68 @@
+import java.awt.Canvas;
 
-public class Game {
+public class Game extends Canvas implements Runnable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean isRunning = false;
+	private Thread thread;
+	
+	private void start() {
+		isRunning = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	private void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now-lastTime)/ns;
+			lastTime = now;
+			while(delta>=1) {
+				tick();
+				delta--;
+			}
+			render();
+			frames++;
+			if(System.currentTimeMillis() - timer>1000) {
+				timer+= 1000;
+				//	System.out.println("FPS: "+frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	private void render() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
