52722c1f0869020f87b9a6d5fac9d6692765a656
yassinedehbi <yassinedehbi4@gmail.com>
2020-04-29 23:08:33 +0000

    "heros 2"

--- Banana/src/Heros.java
+++ Banana/src/Heros.java
@@ -4,59 +4,49 @@ public class Heros {
 	private int id;
 	private int score;
 	private BufferedImage image;
+	private int x;
+	private int y;
+	protected static final int vitesse = 2; 
 	
 	
 	
 	
-	public Heros(int id, int score, BufferedImage image) {
+	public Heros(int id, int score, BufferedImage image, int x, int y) {
 		this.id = id;
 		this.score = score;
 		this.image = image;
+		this.x = x;
+		this.y = y;
 		// TODO Auto-generated constructor stub
 	}
-
-
-
-
 	public int getId() {
 		return id;
 	}
-
-
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
-
-
 	public int getScore() {
 		return score;
 	}
-
-
-
-
-	public void setScore(int score) {
+    public void setScore(int score) {
 		this.score = score;
 	}
-
-
-
-
 	public BufferedImage getImage() {
 		return image;
 	}
-
-
-
-
 	public void setImage(BufferedImage image) {
 		this.image = image;
 	}
-	
-	
-
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
 }
